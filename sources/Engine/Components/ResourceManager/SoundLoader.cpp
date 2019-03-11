d3a78ad12251d3aef8739ca01b166de2d65c0807
Lazarev Yaroslav <yaroslav_lazarev_01@mail.ru>
2019-03-11 21:29:01 +0300

    Added ogg sound loading

--- sources/Engine/Components/ResourceManager/SoundLoader.cpp
+++ sources/Engine/Components/ResourceManager/SoundLoader.cpp
@@ -1,9 +1,12 @@
 #include "SoundLoader.h"
 
 #include "ResourceLoadingException.h"
+#include <filesystem>
+#include <STB/stb_vorbis.c>
 
 #define DR_WAV_IMPLEMENTATION
 #include <dr_libs/dr_wav.h>
+
 
 SoundLoader::SoundLoader()
 {
@@ -14,6 +17,23 @@ SoundLoader::~SoundLoader()
 }
 
 BaseResourceInstance * SoundLoader::load(const std::string & path, std::optional<std::any> options)
+{
+	const std::string expansion = path.substr(path.find_last_of('.')+1);
+	
+	if (expansion == "wav") {
+		return load_format_wav(path, options);
+	}
+	else if (expansion == "ogg") {
+		return load_format_ogg(path, options);
+	}
+	else
+	{
+		throw ResourceLoadingException(ResourceLoadingError::InvalidData, path.c_str(), "File type is wrong", __FILE__, __LINE__, __FUNCTION__);
+	}
+
+}
+
+BaseResourceInstance * SoundLoader::load_format_wav(const std::string & path, std::optional<std::any> options)
 {
 	drwav* pWav = drwav_open_file(path.c_str());
 
@@ -35,6 +55,27 @@ BaseResourceInstance * SoundLoader::load(const std::string & path, std::optional
 	Sound* loaded = new Sound(pSampleData, (size_t)pWav->channels, audioFileSize, sampleRate);
 	drwav_close(pWav);
 	delete pSampleData;
+
+	return new ResourceInstance<Sound>(loaded);
+}
+
+BaseResourceInstance * SoundLoader::load_format_ogg(const std::string & path, std::optional<std::any> options)
+{
+	int channels, audioFileSize, sampleRate;
+	std::byte *decoded;
+
+	if (!std::filesystem::exists(path)) {
+		throw ResourceLoadingException(ResourceLoadingError::InvalidData, path.c_str(), "File doesn't exist", __FILE__, __LINE__, __FUNCTION__);
+	}
+
+	audioFileSize = stb_vorbis_decode_filename(path.c_str(), &channels, &sampleRate, (short**)&decoded);
+
+	if (channels > 2) {
+		throw ResourceLoadingException(ResourceLoadingError::InvalidData, path.c_str(), "The sound file has more than 2 audio channels", __FILE__, __LINE__, __FUNCTION__);
+	}
+
+	Sound* loaded = new Sound(decoded, (size_t)channels, audioFileSize*channels * sizeof(short), sampleRate);
+	delete decoded;
 
 	return new ResourceInstance<Sound>(loaded);
 }
--- sources/Engine/Components/ResourceManager/SoundLoader.h
+++ sources/Engine/Components/ResourceManager/SoundLoader.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include<string>
 #include<Engine/Components/Sound/Sound.h>
 
@@ -10,4 +11,7 @@ public:
 	SoundLoader();
 	virtual ~SoundLoader();
 	virtual BaseResourceInstance* load(const std::string& path, std::optional<std::any> options);
+private:
+	BaseResourceInstance* load_format_wav(const std::string& path, std::optional<std::any> options);
+	BaseResourceInstance* load_format_ogg(const std::string& path, std::optional<std::any> options);
 };
